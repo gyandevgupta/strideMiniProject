158feb415ec03267edac74f6113f8d0eb98a07a9
gyandevgupta <gyandevgupta2000@gmail.com>
2021-08-13 16:10:57 +0530

    add xacro and rviz file

---
+++ simulation_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/gd/simulation_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/gd/simulation_ws/devel/lib:$LD_LIBRARY_PATH"
+export PWD='/home/gd/simulation_ws/build'
+export PYTHONPATH='/opt/ros/melodic/lib/python2.7/dist-packages:/home/gd/catkin_ws/devel/lib/python3/dist-packages'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/gd/simulation_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/gd/simulation_ws/src:$ROS_PACKAGE_PATH"
